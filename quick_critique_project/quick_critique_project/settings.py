683b46f172ccd1f2aea446f704eefa0b531e8672
Sam <silversten@gmail.com>
2024-06-26 15:07:50 +0800

    re-enable admin panel, small changes and fixes

--- quick_critique_project/quick_critique_project/settings.py
+++ quick_critique_project/quick_critique_project/settings.py
@@ -45,7 +45,7 @@ if DEBUG:
 # Application definition
 
 INSTALLED_APPS = [
-    #"django.contrib.admin",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
