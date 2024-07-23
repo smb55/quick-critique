c047286823b3947bfacbd35cf18c5e62d4b663f0
Sam <silversten@gmail.com>
2024-07-23 16:31:13 +0800

    re order imports

--- quick_critique_project/quick_critique_project/__init__.py
+++ quick_critique_project/quick_critique_project/__init__.py
@@ -1,5 +1,5 @@
-from typing import List, Optional
 from __future__ import absolute_import, unicode_literals
+from typing import List, Optional
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
